69edae9ed1965e9dde0e15f603fa2aadc729a0a2
Paul Famiglietti <p0seid0n_@hotmail.com>
2013-11-02 20:32:06 -0700

    Remove option 2

--- index.php
+++ index.php
@@ -50,40 +50,7 @@ speech.onwebkitspeechchange = function(e) {
 
 
 <h1>Option 2</h1>
-<textarea id="textarea" rows=10 cols=80></textarea>
- <button id="button" onclick="toggleStartStop()"></button>
-
- <script type="text/javascript">
-   var recognizing;
-   var recognition = new SpeechRecognition();
-   recognition.continuous = true;
-   reset();
-   recognition.onend = reset;
-
-   recognition.onresult = function (event) {
-     for (var i = resultIndex; i < event.results.length; ++i) {
-       if (event.results.final) {
-         textarea.value += event.results[i][0].transcript;
-       }
-     }
-   }
-
-   function reset() {
-     recognizing = false;
-     button.innerHTML = "Click to Speak";
-   }
-
-   function toggleStartStop() {
-     if (recognizing) {
-       recognition.stop();
-       reset();
-     } else {
-       recognition.start();
-       recognizing = true;
-       button.innerHTML = "Click to Stop";
-     }
-   }
- </script>
+<br><br><br><br>
 
 <h1>Option 3</h1>
 <script type="text/javascript">
